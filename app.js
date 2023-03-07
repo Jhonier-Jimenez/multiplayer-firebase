5f7db67d8af38efdd8c60d3c9393e1580812e9bd
Jhonier-Jimenez <jhonier.jimenez@udea.edu.co>
2023-03-07 13:21:10 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+// Options for Player Colors... these are in the same order as our sprite sheet
+const playerColors = ["blue", "red", "orange", "yellow", "green", "purple"];
+
+//Misc Helpers
+function randomFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+function getKeyString(x, y) {
+  return `${x}x${y}`;
+}
+
+function createName() {
+  const prefix = randomFromArray([
+    "COOL",
+    "SUPER",
+    "HIP",
+    "SMUG",
+    "COOL",
+    "SILKY",
+    "GOOD",
+    "SAFE",
+    "DEAR",
+    "DAMP",
+    "WARM",
+    "RICH",
+    "LONG",
+    "DARK",
+    "SOFT",
+    "BUFF",
+    "DOPE",
+  ]);
+  const animal = randomFromArray([
+    "BEAR",
+    "DOG",
+    "CAT",
+    "FOX",
+    "LAMB",
+    "LION",
+    "BOAR",
+    "GOAT",
+    "VOLE",
+    "SEAL",
+    "PUMA",
+    "MULE",
+    "BULL",
+    "BIRD",
+    "BUG",
+  ]);
+  return `${prefix} ${animal}`;
+}
+
+
+(function () {
+  
+  let playerId;
+  let playerRef;
+
+  const name = createName()
+
+
+  firebase.auth().onAuthStateChanged((user) => {
+    console.log(user)
+    if (user) {
+      playerId = user.uid;
+      playerRef = firebase.database().ref(`players/${playerId}`);
+
+      playerRef.set({
+        id:playerId,
+        name,
+        color: randomFromArray(playerColors)
+
+      })
+
+      playerRef.onDisconnect().remove();
+    } else {
+      
+    }
+  })
+
+  firebase.auth().signInAnonymously().catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    console.log(errorCode, errorMessage);
+  });
+}
+)();
